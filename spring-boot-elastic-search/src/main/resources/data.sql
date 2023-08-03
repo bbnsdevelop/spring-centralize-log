5bad1a0ddebf230e1355950874ba96d44b80b52f
bbnsdevelop <bbnsdevelop@gmail.com>
2023-08-02 22:56:08 -0300

    insert data in table TB_STATE

---
+++ spring-boot-elastic-search/src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO TB_STATE (id, region, name) VALUES (1, 'Noth', 'Acre');
+INSERT INTO TB_STATE (id, region, name) VALUES (2, 'Noth', 'Amazonas');
+INSERT INTO TB_STATE (id, region, name) VALUES (3, 'Noth', 'Roraima');
+INSERT INTO TB_STATE (id, region, name) VALUES (4, 'Noth', 'Rondônia');
+INSERT INTO TB_STATE (id, region, name) VALUES (5, 'Noth', 'Pará');
+INSERT INTO TB_STATE (id, region, name) VALUES (6, 'Noth', 'Amapá');
+INSERT INTO TB_STATE (id, region, name) VALUES (7, 'Noth', 'Tocantins');
+
+INSERT INTO TB_STATE (id, region, name) VALUES (8, 'Northeast', 'Maranhão');
+INSERT INTO TB_STATE (id, region, name) VALUES (9, 'Northeast', 'Piauí');
+INSERT INTO TB_STATE (id, region, name) VALUES (10, 'Northeast', 'Ceará');
+INSERT INTO TB_STATE (id, region, name) VALUES (11, 'Northeast', 'Rio Grande do Norte');
+INSERT INTO TB_STATE (id, region, name) VALUES (12, 'Northeast', 'Paraíba');
+INSERT INTO TB_STATE (id, region, name) VALUES (13, 'Northeast', 'Pernambuco');
+INSERT INTO TB_STATE (id, region, name) VALUES (14, 'Northeast', 'Alagoas');
+INSERT INTO TB_STATE (id, region, name) VALUES (15, 'Northeast', 'Sergipe');
+INSERT INTO TB_STATE (id, region, name) VALUES (16, 'Northeast', 'Bahia');
+INSERT INTO TB_STATE (id, region, name) VALUES (17, 'Central-West', 'Mato Grosso');
+INSERT INTO TB_STATE (id, region, name) VALUES (18, 'Central-West', 'Mato Grosso do Sul');
+INSERT INTO TB_STATE (id, region, name) VALUES (19, 'Central-West', 'Goiás');
+INSERT INTO TB_STATE (id, region, name) VALUES (20, 'Central-West', 'Brasília');
+INSERT INTO TB_STATE (id, region, name) VALUES (21, 'South Region', 'Paraná');
+INSERT INTO TB_STATE (id, region, name) VALUES (22, 'South Region', 'Santa Catarina');
+INSERT INTO TB_STATE (id, region, name) VALUES (23, 'South Region', 'Rio Grande do Sul');
+INSERT INTO TB_STATE (id, region, name) VALUES (24, 'Southeast', 'Minas Gerais');
+INSERT INTO TB_STATE (id, region, name) VALUES (25, 'Southeast', 'Espírito Santo');
+INSERT INTO TB_STATE (id, region, name) VALUES (26, 'Southeast', 'São Paulo');
+INSERT INTO TB_STATE (id, region, name) VALUES (27, 'Southeast', 'Rio de Janeiro');
+
+
